35fa3bac35bc6b4300367b2dc1b6fae5788fea83
valeryvaladzko <hwoarangfreak@gmail.com>
2017-07-19 16:49:55 +0300

    2.2 JS done (90%)

--- 2.2JS/js/main.js
+++ 2.2JS/js/main.js
@@ -2,81 +2,70 @@ window.onload = function () {
   document.getElementById('1').onclick = checkStr
 }
 // глобальные переменные
-var strDate
-var strMask
-var strFormat
-var strResult
-var arrAll
+var strDate // строка даты
+var strMask // строка маски
+var strFormat // строка выходного формата
+var strResult // строка результата
+var arrDate // массив даты
+var arrMask // массив маски
+var arrFormat // массив выходного формата
+
 var obj = {
-  'DDMMYYYY': function (strDate) {
+  '1': function (strDate) {
     var arrResult = []
-    arrResult[0] = strDate.slice(0, 2) // День
-    arrResult[1] = strDate.slice(2, 4) // Месяц
-    arrResult[2] = strDate.slice(4, 8) // Год
+    arrResult.push(strDate.slice(0, 2))  // День
+    arrResult.push(strDate.slice(2, 4)) // Месяц
+    arrResult.push(strDate.slice(4, 8)) // Год
     strResult = arrResult.join('-')
     document.getElementById('output').innerHTML = '<span>' + strResult + '</span>'
   },
-  'YYYYMMDD': function (strDate) {
+  '2': function () {
+    var arrD = [] // массив дней
+    var arrM = [] // массив месяца
+    var arrY = [] // массив года
+    var arrS = [] // массив сепаратора
+    for (var i = 0, length = arrDate.length; i < length; i++) {
+      if (arrMask[i] === 'D') {
+        arrD.push(arrDate[i])
+      } else if (arrMask[i] === 'M') {
+        arrM.push(arrDate[i])
+      } else if (arrMask[i] === 'Y') {
+        arrY.push(arrDate[i])
+      } else {
+        arrS.push(arrDate[i])
+      }
+    }
     var arrResult = []
-    arrResult[0] = strDate.slice(0, 4) // Год
-    arrResult[1] = strDate.slice(4, 6) // Месяц
-    arrResult[2] = strDate.slice(6, 8) // День
-    strResult = arrResult.join('-')
+    for (i = 0, length = arrFormat.length; i < length; i++) {
+      if (arrFormat[i] === 'D') {
+        arrResult.push(arrD[0])
+        arrD.shift()
+      } else if (arrFormat[i] === 'M') {
+        arrResult.push(arrM[0])
+        arrM.shift()
+      } else if (arrFormat[i] === 'Y') {
+        arrResult.push(arrY[0])
+        arrY.shift()
+      } else {
+        arrResult.push(arrFormat[i])
+      }
+    }
+    strResult = arrResult.join('')
     document.getElementById('output').innerHTML = '<span>' + strResult + '</span>'
-  },
-  'DD-MM-YYYY': function (strDate) {
-    console.log('DD-MM-YYYY mask')
-  },
-  'YYYY-MM-DD': function (strDate) {
-    console.log('YYYY-MM-DD mask')
-  },
-  'D-M-YYYY': function (strDate) {
-    console.log('D-M-YYYY mask')
-  },
-  'YYYY-M-D': function (strDate) {
-    console.log('YYYY-M-D mask')
   }
 }
 
 function checkStr () {
-  //проверяем введенные данные
-  var strInput = document.getElementById('input').value
-  arrAll = strInput.split(',')
-  var length = arrAll.length
-  strDate = arrAll[0]
-  strMask = arrAll[1]
-  strFormat = arrAll[2]
-  if (length === 1) {
-    convertDate1()
+  // проверяем введенные данные
+  strDate = document.getElementById('date').value
+  arrDate = strDate.split('')
+  strMask = document.getElementById('mask').value
+  arrMask = strMask.split('')
+  strFormat = document.getElementById('format').value
+  arrFormat = strFormat.split('')
+  if (strMask === '') {
+    obj['1'](strDate)
+  } else {
+    obj['2'](strDate)
   }
-  if (length === 2) {
-    obj[strMask](strDate)
-  }
-  if (length === 3) {
-    convertDate3()
-  }
-}
-
-function convertDate1 () {
-  var str = arrAll[0]
-  var arrTemp = []
-  arrTemp[0] = str.slice(0, 4) // Год
-  arrTemp[1] = str.slice(4, 6) // Месяц
-  arrTemp[2] = str.slice(6, 8) // День
-  str = arrTemp.join('-')
-  document.getElementById('output').innerHTML = '<span>' + str + '</span>'
-}
-
-//   пока не доделано
-function convertDate3 () {
-  var strInput = document.getElementById('input').value
-  var arrAll = strInput.split(',')
-  // вычленяем дату
-  strDate = arrAll[0]
-  strMask = arrAll[1]
-  strFormat = arrAll[2]
-  obj[strMask]()
-  // ищем совпадение по маске
-  var now = new Date()
-  document.getElementById('output').innerHTML = '<span>' + now + '</span>'
 }
