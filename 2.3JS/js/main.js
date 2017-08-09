95e2affbc5fb7c899045c671b81ad175e62e74d5
valeryvaladzko <hwoarangfreak@gmail.com>
2017-08-09 16:04:13 +0300

    2.3 fixed after review

--- 2.3JS/js/main.js
+++ 2.3JS/js/main.js
@@ -1,53 +1,66 @@
-window.onload = function () {
-  document.getElementById('text-formatter').onclick = textFormatter;
-};
+var text = $('#text')[0];
+var maxLength = $('#max-length')[0];
+var maxCount = $('#max-count')[0];
+$('#text-formatter').on('click', textFormatter);
 
 function textFormatter () {
-  var obj = {
-    'word': function () {
-      var counter = 0;
-      var counterAll = 0;
-      var arrResult = [];
-      var j = 0;
-      for (var i = 0, length = strMain.length; i < length; i++) {
-        if (strMain[i] === ' ') {
-          var y = strMain.substring(counterAll, counterAll + counter);
-          arrResult[j] = y;
-          j++;
-          counterAll += counter + 1;
-          counter = 0;
-        } else {
-          counter++;
-        }
+  var arrRadio = $('input:checked').val();
+  switch (arrRadio) {
+    case 'letter': separateByLetter(); break;
+    case 'word': separateByWord(); break;
+    case 'without': separateByNothing(); break;
+    case 'sentence': separateBySentence(); break;
+    default: alert('Null? Oo'); break;
+  }
+}
+
+function separateByLetter() {
+  var strMain = text.value;
+  var strLength = parseInt(maxLength.value);
+  var strCount = parseInt(maxCount.value);
+  var arrResult = [];
+  for (var i = 0, k = 0; k < strCount; k++) {
+    arrResult[k] = strMain.substr(i, strLength);
+    i += strLength;
+    console.log(arrResult[k]);
+  }
+}
+
+function separateByWord() {
+  var strMain = text.value;
+  var strLength = parseInt(maxLength.value);
+  var strCount = parseInt(maxCount.value);
+  var arrMain = strMain.split(' ');
+  var strRes = '';
+  for (var i = 0, k = 0; i < arrMain.length && k < strCount; i++) {
+    if (strRes.length < strLength) {
+      strRes += ' ' + arrMain[i];
+      if (i+1 === arrMain.length) {
+        console.log(strRes);
       }
-      var strTempAll = '';
-      var countS = 0;
-      for (i = 0, length = arrResult.length; i < length; i++) {
-        strTempAll += arrResult[i] + ' ';
-        if ((strTempAll + arrResult[i + 1]).length > strLength && countS < strCount) {
-          var s = document.createElement('span');
-          s.innerHTML = '<span>' + strTempAll + '<br>' + '</span>';
-          document.getElementById('output').appendChild(s);
-          strTempAll = '';
-          countS++;
-        }
-      }
-    },
-    'letter': function () {
-    },
-    'sentence': function () {
-    },
-    'without': function () {
-    }
-  };
-  var strMain = document.getElementById('text').value;
-  var strLength = document.getElementById('max-length').value;
-  var strCount = document.getElementById('max-count').value;
-  var arrRadio = document.getElementsByName('separator');
-  for (var i = 0, length = arrRadio.length; i < length; i++) {
-    if (arrRadio[i].checked) {
-      var value = arrRadio[i].value;
-      obj[value]();
+    } else {
+      console.log(strRes);
+      strRes = arrMain[i];
+      k++;
     }
   }
+}
+
+function separateBySentence() {
+  var strMain = text.value;
+  var strCount = parseInt(maxCount.value);
+  var arrMain = strMain.split('.');
+  var strRes = '';
+  for (var i = 0, k = 0; i < arrMain.length && k < strCount; i++) {
+    strRes += arrMain[i] + '.';
+    console.log(strRes.trim());
+    strRes = '';
+    k++;
+  }
+}
+
+
+function separateByNothing() {
+  var strMain = text.value;
+  console.log(strMain);
 }
