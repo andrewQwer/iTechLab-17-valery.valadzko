12e06fa681884ce10b1f730482f01a90ac013795
valeryvaladzko <hwoarangfreak@gmail.com>
2017-08-09 14:19:52 +0300

    2.2 fixed after review

--- 2.2JS/js/main.js
+++ 2.2JS/js/main.js
@@ -10,50 +10,89 @@ var arrDate; // массив даты
 var arrMask; // массив маски
 var arrFormat; // массив выходного формата
 
-var obj = {
-  '1': function (strDate) {
-    var arrResult = [];
-    arrResult.push(strDate.slice(0, 2));  // День
-    arrResult.push(strDate.slice(2, 4)); // Месяц
-    arrResult.push(strDate.slice(4, 8)); // Год
-    strResult = arrResult.join('-');
-    document.getElementById('output').innerHTML = '<span>' + strResult + '</span>';
-  },
-  '2': function () {
-    var arrD = []; // массив дней
-    var arrM = []; // массив месяца
-    var arrY = []; // массив года
-    var arrS = []; // массив сепаратора
-    for (var i = 0, length = arrDate.length; i < length; i++) {
-      if (arrMask[i] === 'D') {
-        arrD.push(arrDate[i]);
-      } else if (arrMask[i] === 'M') {
-        arrM.push(arrDate[i]);
-      } else if (arrMask[i] === 'Y') {
-        arrY.push(arrDate[i]);
-      } else {
-        arrS.push(arrDate[i]);
-      }
+function convertWithoutMask() {
+  var arrResult = [];
+  arrResult.push(strDate.slice(0, 2));  // День
+  arrResult.push(strDate.slice(2, 4)); // Месяц
+  arrResult.push(strDate.slice(4, 8)); // Год
+  strResult = arrResult.join('-');
+  document.getElementById('output').innerHTML = '<span>Результат: ' + strResult + '</span>';
+}
+
+function convertWithoutFormat() {
+  var arrD = []; // массив дней
+  var arrM = []; // массив месяца
+  var arrY = []; // массив года
+  var arrS = []; // массив сепаратора
+  for (var i = 0, length = arrDate.length; i < length; i++) {
+    if (arrMask[i] === 'D') {
+      arrD.push(arrDate[i]);
+    } else if (arrMask[i] === 'M') {
+      arrM.push(arrDate[i]);
+    } else if (arrMask[i] === 'Y') {
+      arrY.push(arrDate[i]);
+    } else {
+      arrS.push(arrDate[i]);
     }
-    var arrResult = [];
-    for (i = 0, length = arrFormat.length; i < length; i++) {
-      if (arrFormat[i] === 'D') {
-        arrResult.push(arrD[0]);
-        arrD.shift();
-      } else if (arrFormat[i] === 'M') {
-        arrResult.push(arrM[0]);
-        arrM.shift();
-      } else if (arrFormat[i] === 'Y') {
-        arrResult.push(arrY[0]);
-        arrY.shift();
-      } else {
-        arrResult.push(arrFormat[i]);
-      }
-    }
-    strResult = arrResult.join('');
-    document.getElementById('output').innerHTML = '<span>' + strResult + '</span>';
   }
-};
+  var strM = arrM.join('');
+  switch (strM) {
+    case '01': strM = 'January'; break;
+    case '02': strM = 'February'; break;
+    case '03': strM = 'March'; break;
+    case '04': strM = 'April'; break;
+    case '05': strM = 'May'; break;
+    case '06': strM = 'June'; break;
+    case '07': strM = 'July'; break;
+    case '08': strM = 'August'; break;
+    case '09': strM = 'September'; break;
+    case '10': strM = 'October'; break;
+    case '11': strM = 'November'; break;
+    case '12': strM = 'December'; break;
+    default: strM = null; break;
+  }
+  strResult = arrD.join('') + ' ' + strM + ' ' + arrY.join('');
+  var now = new Date();
+  var year = now.getFullYear();
+  var fromNow = year - arrY.join('');
+  document.getElementById('output').innerHTML = '<span>Результат: ' + ((strM === null) ? 'Not correct month' : strResult) + '</span>';
+  document.getElementById('output2').innerHTML = '<span>' + fromNow + ' years ago';
+}
+
+function convertWithAll() {
+  var arrD = []; // массив дней
+  var arrM = []; // массив месяца
+  var arrY = []; // массив года
+  var arrS = []; // массив сепаратора
+  var arrResult = [];
+  for (var i = 0, length = arrDate.length; i < length; i++) {
+    if (arrMask[i] === 'D') {
+      arrD.push(arrDate[i]);
+    } else if (arrMask[i] === 'M') {
+      arrM.push(arrDate[i]);
+    } else if (arrMask[i] === 'Y') {
+      arrY.push(arrDate[i]);
+    } else {
+      arrS.push(arrDate[i]);
+    }
+  }
+  for (i = 0, length = arrFormat.length; i < length; i++) {
+    if (arrFormat[i] === 'D') {
+      arrResult.push(arrD[0]);
+      arrD.shift();
+    } else if (arrFormat[i] === 'M') {
+      arrResult.push(arrM[0]);
+      arrM.shift();
+    } else if (arrFormat[i] === 'Y') {
+      arrResult.push(arrY[0]);
+      arrY.shift();
+    } else {
+      arrResult.push(arrFormat[i]);
+    }
+  }
+  strResult = arrResult.join('');
+  document.getElementById('output').innerHTML = '<span>Результат: ' + strResult + '</span>';
+}
 
 function checkStr () {
   // проверяем введенные данные
@@ -64,8 +103,10 @@ function checkStr () {
   strFormat = document.getElementById('format').value;
   arrFormat = strFormat.split('');
   if (strMask === '') {
-    obj['1'](strDate);
+    convertWithoutMask(strDate);
+  } else if (strFormat === '') {
+    convertWithoutFormat(strDate);
   } else {
-    obj['2'](strDate);
+    convertWithAll();
   }
 }
