cdfe173aaf3f82862211b88e6f48a5132543e5da
valeryvaladzko <hwoarangfreak@gmail.com>
2017-08-10 14:27:10 +0300

    2.4 fixed after review

--- 2.4JS/js/main.js
+++ 2.4JS/js/main.js
@@ -1,38 +1,79 @@
-window.onload = function () {
-  document.getElementById('count').onclick = textFormatter;
-  var num = document.getElementsByClassName('arr-form__option-num');
-  for (var i = 0; i < num.length; i++) {
-    num[i].addEventListener('keyup', checkSymbol);
-  }
-  function checkSymbol () {
-    var num = document.getElementsByClassName('arr-form__option-num');
-    for (var i = 0; i < num.length; i++) {
-      num[i].value = num[i].value.replace(/[^.0-9]/g, '');
-    }
-  }
-};
+$('#count').on('click', stringCalculator);
+var exp = $('#expression')[0];
+exp.addEventListener('keyup', checkSymbol);
+function checkSymbol () {
+  exp.value = exp.value.replace(/[^-*/+.0-9()]/g, '');
+}
 
-function textFormatter () {
-  var a = document.getElementById('a').value;
-  var b = document.getElementById('b').value;
-  var operation = document.getElementById('operation').value;
-  var obj = {
-    '+': function () {
-      var result = +a + +b;
-      document.getElementById('output').innerHTML = '<span>' + result + '</span>';
-    },
-    '-': function () {
-      var result = +a - +b;
-      document.getElementById('output').innerHTML = '<span>' + result + '</span>';
-    },
-    '*': function () {
-      var result = +a * +b;
-      document.getElementById('output').innerHTML = '<span>' + result + '</span>';
-    },
-    '/': function () {
-      var result = +a / +b;
-      document.getElementById('output').innerHTML = '<span>' + result + '</span>';
+function stringCalculator () {
+  var mainExp = exp.value;
+  var arrExp = mainExp.split('');
+  var a = '';
+  var arrRes = [];
+  var flag = false;
+  for (var i = 0, length = arrExp.length; i < length; i++) {
+    if (flag) {
+      a += arrExp[i];
+      if (arrExp[i + 1] === ')') {
+        arrRes.push(parseInt(a));
+        flag = false;
+        i++;
+        a = '';
+      }
+    } else {
+      if (isNaN(parseInt(arrExp[i]))) {
+        if (arrExp[i] === '(') {
+          flag = true;
+        } else {
+          arrRes.push(arrExp[i]);
+        }
+      } else {
+        if (isNaN(parseInt(arrExp[i + 1]))) {
+          a += arrExp[i];
+          arrRes.push(parseInt(a));
+          a = '';
+        } else {
+          a += arrExp[i];
+        }
+      }
     }
-  };
-  obj[operation]();
+  }
+  var temp = 0;
+  console.log('arrRes: ', arrRes);
+  for (i = 0; i < arrRes.length; i++) {
+    if (arrRes[i] === '*') {
+      temp = arrRes[i-1] * arrRes[i+1];
+      arrRes.splice(i-1, 3, temp);
+      i=0;
+      console.log('arrRes: ', arrRes);
+    }
+  }
+  console.log('Before div: ', arrRes);
+  for (i = 0; i < arrRes.length; i++) {
+    if (arrRes[i] === '/') {
+      temp = arrRes[i-1] / arrRes[i+1];
+      arrRes.splice(i-1, 3, temp);
+      i=0;
+      console.log('arrRes: ', arrRes);
+    }
+  }
+  console.log('Before plus: ', arrRes);
+  for (i = 0; i < arrRes.length; i++) {
+    if (arrRes[i] === '+') {
+      temp = arrRes[i-1] + arrRes[i+1];
+      arrRes.splice(i-1, 3, temp);
+      i=0;
+      console.log('arrRes: ', arrRes);
+    }
+  }
+  console.log('Before minus: ', arrRes);
+  for (i = 0; i < arrRes.length; i++) {
+    if (arrRes[i] === '-') {
+      temp = arrRes[i-1] - arrRes[i+1];
+      arrRes.splice(i-1, 3, temp);
+      i=0;
+      console.log('arrRes: ', arrRes);
+    }
+  }
+  $('#output').text(arrRes);
 }
