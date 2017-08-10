9800373effa0ee6ad93116f47b8504a3711a997f
valeryvaladzko <hwoarangfreak@gmail.com>
2017-08-10 16:58:43 +0300

    4.1 fixed after review

--- 4.1Callback/main.js
+++ 4.1Callback/main.js
@@ -1,62 +1,94 @@
-var getCity = function (callback) {
-  var cityname = document.getElementById('city').value;
-  let location = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + cityname + '") and u="c"';
-  let url = 'https://query.yahooapis.com/v1/public/yql?q=' + location + '&format=json';
-  return callback(url);
-};
+let weatherDom = $('#weather');
+let bynDom = $('#BYN');
+let eurDom = $('#EUR');
+let city = $('#city');
+let updateWeather = $('#update_weather');
+let updateCurrency = $('#update_currency');
 
-var requestWeather = function(url, callback) {
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.send();
-  xhr.onreadystatechange = function () {
-    if (xhr.status === 200 && xhr.readyState === 4) {
-      let weatherObj = JSON.parse(xhr.responseText);
-      console.log('Weather object: ', weatherObj);
-      return callback(weatherObj);
+class Yahoo {
+  constructor (type) {
+    this.type = type;
+  }
+  getUrlFromCity (callback) {
+    switch (this.type) {
+      case 'Weather': {
+        var cityname = city.val();
+        let location = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + cityname + '") and u="c"';
+        let url = 'https://query.yahooapis.com/v1/public/yql?q=' + location + '&format=json';
+        return callback(url);
+      }
+      case 'Currency': {
+        let url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDBYN%22%2C%20%22USDEUR%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+        return callback(url);
+      }
+      default: {
+        alert('Error on get city');
+        break;
+      }
     }
   };
-};
 
-var setWeather = function (weatherObj) {
-  let weather = document.getElementById('weather');
-  weather.innerHTML = weatherObj.query.results.channel.item.condition.temp;
-};
+  requestJson (url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+    xhr.onreadystatechange = function () {
+      if (xhr.status === 200 && xhr.readyState === 4) {
+        let jsonObj = JSON.parse(xhr.responseText);
+        console.log(this.type + ' jsonObject: ', jsonObj);
+        return callback(jsonObj);
+      }
+    };
+  };
 
-var requestCurrency = function(callback) {
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDBYN%22%2C%20%22USDEUR%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=');
-  xhr.send();
-  xhr.onreadystatechange = function () {
-    if (xhr.status === 200 && xhr.readyState === 4) {
-      let currencyObj = JSON.parse(xhr.responseText);
-      console.log('Currency object: ', currencyObj);
-      return callback(currencyObj);
+  setDom (jsonObj) {
+    switch (this.type) {
+      case 'Weather': {
+        weatherDom.text(jsonObj.query.results.channel.item.condition.temp);
+        break;
+      }
+      case 'Currency': {
+        bynDom.text(jsonObj.query.results.rate['0'].Rate);
+        eurDom.text(jsonObj.query.results.rate['1'].Rate);
+        break;
+      }
+      default: {
+        alert('Error on set DOM');
+        break;
+      }
     }
   };
-};
 
-var setCurrency = function (currencyObj) {
-  let BYN = document.getElementById('BYN');
-  let EUR = document.getElementById('EUR');
-  BYN.innerHTML = currencyObj.query.results.rate['0'].Rate;
-  EUR.innerHTML = currencyObj.query.results.rate['1'].Rate;
-};
-
-var runWeather = function() {
-  getCity(function (url) {
-    requestWeather(url, function (weatherObj) {
-      setWeather(weatherObj);
+  runAsync () {
+    self.getUrlFromCity(function (url) {
+      self.requestJson(url, function (jsonObj) {
+        self.setDom(jsonObj);
+      });
     });
-  });
+  }
 }
 
-var runCurrency = function() {
-  requestCurrency(function (currencyObj) {
-    setCurrency(currencyObj);
+let yahooWeather = new Yahoo('Weather');
+let yahooCurrency = new Yahoo('Currency');
+
+let runWeather = function() {
+  yahooWeather.getUrlFromCity(function (url) {
+    yahooWeather.requestJson(url, function (jsonObj) {
+      yahooWeather.setDom(jsonObj);
+    });
   });
 };
 
+let runCurrency = function() {
+  yahooCurrency.getUrlFromCity(function (url) {
+    yahooCurrency.requestJson(url, function (jsonObj) {
+      yahooCurrency.setDom(jsonObj);
+    });
+  });
+};
 
-document.getElementById('update_weather').addEventListener('click', runWeather);
-document.getElementById('update_currency').addEventListener('click', runCurrency);
+updateWeather.on('click', runWeather);
+updateCurrency.on('click', runCurrency);
+
+updateWeather.on('click', yahooWeather.runAsync);
+updateCurrency.on('click', yahooCurrency.runAsync);
