e7d53c3d6e413014ef3e578dcd4aa4a466085ce8
valeryvaladzko <hwoarangfreak@gmail.com>
2017-08-11 16:31:16 +0300

    4.1 changes

--- 4.1Callback/main.js
+++ 4.1Callback/main.js
@@ -8,7 +8,71 @@ let updateCurrency = $('#update_currency');
 class Yahoo {
   constructor (type) {
     this.type = type;
-    self = this;
+
+    this.getUrlFromCity = (callback) => {
+      console.log('Type = ', this.type);
+      switch (this.type) {
+        case 'Weather': {
+          var cityname = city.val();
+          let location = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + cityname + '") and u="c"';
+          let url = 'https://query.yahooapis.com/v1/public/yql?q=' + location + '&format=json';
+          console.log(url);
+          console.log(callback);
+
+          return callback(url);
+        }
+        case 'Currency': {
+          let url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDBYN%22%2C%20%22USDEUR%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+          return callback(url);
+        }
+        default: {
+          alert('Error on get city');
+          break;
+        }
+      }
+    };
+
+    this.requestJson = (url, callback) => {
+      console.log('Type = ', this.type);
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.send();
+      xhr.onreadystatechange = function () {
+        if (xhr.status === 200 && xhr.readyState === 4) {
+          let jsonObj = JSON.parse(xhr.responseText);
+          console.log(this.type + ' jsonObject: ', jsonObj);
+          return callback(jsonObj);
+        }
+      };
+    };
+
+    this.setDom = (jsonObj) => {
+      console.log('Type = ', this.type);
+      switch (this.type) {
+        case 'Weather': {
+          weatherDom.text(jsonObj.query.results.channel.item.condition.temp);
+          break;
+        }
+        case 'Currency': {
+          bynDom.text(jsonObj.query.results.rate['0'].Rate);
+          eurDom.text(jsonObj.query.results.rate['1'].Rate);
+          break;
+        }
+        default: {
+          alert('Error on set DOM');
+          break;
+        }
+      }
+    };
+
+    this.runAsync = () => {
+      this.getUrlFromCity (function (url) {
+        this.requestJson(url, function (jsonObj) {
+          this.setDom(jsonObj);
+        });
+      });
+    };
+
   }
 
   getUrlFromCity (callback) {
@@ -66,10 +130,9 @@ class Yahoo {
   }
 
   runAsync () {
-    console.log('Type = ', this.type);
-    self.getUrlFromCity(function (url) {
-      self.requestJson(url, function (jsonObj) {
-        self.setDom(jsonObj);
+    getUrlFromCity (function (url) {
+      requestJson(url, function (jsonObj) {
+        setDom(jsonObj);
       });
     });
   }
