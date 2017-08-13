bc2d8cba6ddee54442881498029e205f00f44156
valeryvaladzko <hwoarangfreak@gmail.com>
2017-08-13 16:31:27 +0300

    4.1 finished

--- 4.1Callback/main.js
+++ 4.1Callback/main.js
@@ -10,15 +10,11 @@ class Yahoo {
     this.type = type;
 
     this.getUrlFromCity = (callback) => {
-      console.log('Type = ', this.type);
       switch (this.type) {
         case 'Weather': {
-          var cityname = city.val();
+          let cityname = city.val();
           let location = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + cityname + '") and u="c"';
           let url = 'https://query.yahooapis.com/v1/public/yql?q=' + location + '&format=json';
-          console.log(url);
-          console.log(callback);
-
           return callback(url);
         }
         case 'Currency': {
@@ -33,21 +29,18 @@ class Yahoo {
     };
 
     this.requestJson = (url, callback) => {
-      console.log('Type = ', this.type);
       let xhr = new XMLHttpRequest();
       xhr.open('GET', url);
       xhr.send();
-      xhr.onreadystatechange = function () {
+      xhr.onreadystatechange = () => {
         if (xhr.status === 200 && xhr.readyState === 4) {
           let jsonObj = JSON.parse(xhr.responseText);
-          console.log(this.type + ' jsonObject: ', jsonObj);
           return callback(jsonObj);
         }
       };
     };
 
     this.setDom = (jsonObj) => {
-      console.log('Type = ', this.type);
       switch (this.type) {
         case 'Weather': {
           weatherDom.text(jsonObj.query.results.channel.item.condition.temp);
@@ -66,75 +59,13 @@ class Yahoo {
     };
 
     this.runAsync = () => {
-      this.getUrlFromCity (function (url) {
-        this.requestJson(url, function (jsonObj) {
+      this.getUrlFromCity((url) => {
+        this.requestJson(url, (jsonObj) => {
           this.setDom(jsonObj);
         });
       });
     };
 
-  }
-
-  getUrlFromCity (callback) {
-    console.log('Type = ', this.type);
-    switch (this.type) {
-      case 'Weather': {
-        var cityname = city.val();
-        let location = 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + cityname + '") and u="c"';
-        let url = 'https://query.yahooapis.com/v1/public/yql?q=' + location + '&format=json';
-        console.log(url);
-        return callback(url);
-      }
-      case 'Currency': {
-        let url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDBYN%22%2C%20%22USDEUR%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
-        return callback(url);
-      }
-      default: {
-        alert('Error on get city');
-        break;
-      }
-    }
-  }
-
-  requestJson (url, callback) {
-    console.log('Type = ', this.type);
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send();
-    xhr.onreadystatechange = function () {
-      if (xhr.status === 200 && xhr.readyState === 4) {
-        let jsonObj = JSON.parse(xhr.responseText);
-        console.log(this.type + ' jsonObject: ', jsonObj);
-        return callback(jsonObj);
-      }
-    };
-  }
-
-  setDom (jsonObj) {
-    console.log('Type = ', this.type);
-    switch (this.type) {
-      case 'Weather': {
-        weatherDom.text(jsonObj.query.results.channel.item.condition.temp);
-        break;
-      }
-      case 'Currency': {
-        bynDom.text(jsonObj.query.results.rate['0'].Rate);
-        eurDom.text(jsonObj.query.results.rate['1'].Rate);
-        break;
-      }
-      default: {
-        alert('Error on set DOM');
-        break;
-      }
-    }
-  }
-
-  runAsync () {
-    getUrlFromCity (function (url) {
-      requestJson(url, function (jsonObj) {
-        setDom(jsonObj);
-      });
-    });
   }
 
 }
