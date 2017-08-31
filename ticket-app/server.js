d9f3d1ec2212ab47debed6f3bde9b8f1d9415cc8
valeryvaladzko <hwoarangfreak@gmail.com>
2017-08-31 16:26:37 +0300

    backend

--- ticket-app/server.js
+++ ticket-app/server.js
@@ -1,23 +1,32 @@
-let webpack = require('webpack');
-let webpackDevMiddleware = require('webpack-dev-middleware');
-let webpackHotMiddleware = require('webpack-hot-middleware');
-let config = require('./webpack.config');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config');
+const compiler = webpack(config);
 
-let app = new (require('express'))();
-let port = 3000;
+const express = require('express');
+const Mongoclient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const db = require('./server/config/db');
+const app = express();
+const port = 3000;
 
-let compiler = webpack(config);
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
 app.use(webpackHotMiddleware(compiler));
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + '/index.html')
+Mongoclient.connect(db.url, (err, database) => {
+    if (err) {return console.log(err)}
+    require('./server/routes')(app, database);
+    app.listen(port, () => {
+        console.log('Live on port ' + port);
+    })
 });
 
-app.listen(port, function(error) {
-    if (error) {
-        console.error(error)
-    } else {
-        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
-    }
+
+
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/index.html')
 });
